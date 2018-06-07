79a5174e8057f440f43bf22a109b140e0e78c026
Duncan <duncanjmr@gmail.com>
2018-06-06 23:25:22 -0600

    Improved Coverage and Fixed Docstrings 	modified:   examples/Run_Jackknife.ipynb 	deleted:    hera_stats/data/gauss.spl_ants.Nj40.2018-06-05.21_21_17.jkf 	new file:   hera_stats/data/gauss.spl_ants.Nj40.2018-06-07.04_50_54.jkf 	modified:   hera_stats/jackknife.py 	modified:   hera_stats/jkf_stats.py 	modified:   hera_stats/plots.py 	deleted:    hera_stats/test.py 	renamed:    hera_stats/tests/test_stats.py -> hera_stats/tests/Test_stats.py 	modified:   hera_stats/tests/test_jackknife.py 	modified:   hera_stats/utils.py

--- hera_stats/tests/Test_stats.py
+++ hera_stats/tests/Test_stats.py
@@ -1,5 +1,3 @@
-import matplotlib
-matplotlib.use("Agg",warn=False)
 import hera_stats as hs
 import matplotlib.pyplot as plt
 import nose.tools as nt
@@ -8,53 +6,64 @@ import nose.tools as nt
 class Test_Plots():
 
     def setUp(self):
-        self.filepath = "./hera_stats/data/gauss.spl_ants.Nj40.2018-06-05.21_21_17.jkf"
+        self.filepath = "./hera_stats/data/gauss.spl_ants.Nj40.2018-06-07.04_50_54.jkf"
         plt.ioff()
-        
+
         pl = hs.plots()
         pl.load_file(self.filepath)
-        
         self.pl = pl
-    
+
     def test_init(self):
-        
+
         pl = hs.plots()
-        
+
         nt.assert_raises(IOError, pl.load_file, "nothing")
-        
+
         pl.load_file(self.filepath)
         nt.assert_is_instance(pl.stats, hs.jkf_stats)
         nt.assert_is_instance(pl.data, hs.utils.jkfdata)
-    
+
     def test_plots(self):
-        
+
         f = plt.figure()
-        
+        ax = f.add_subplot(111)
         pl = self.pl
-        
+
         pl.plot_spectra(fig=f)
-        plt.close()
         pl.plot_spectra(fig=f,show_groups=True)
-        plt.close()
         nt.assert_raises(ValueError,pl.plot_spectra,100,fig=f)
-        
+
         pl.plot_spectra_errs(fig=f)
-        plt.close()
         pl.plot_spectra_errs(fig=f,show_groups=True)
-        plt.close()
         nt.assert_raises(ValueError,pl.plot_spectra_errs,100,fig=f)
-        plt.close()
-        
+        f.clear()
+
         for mode in ["varsum","weightedsum","raw","norm"]:
-            pl.hist_2d(plottype = mode)
-            plt.close()
-        
+            pl.hist_2d(ax=ax,plottype = mode)
+            ax.clear()
+
         nt.assert_raises(NameError, pl.hist_2d, "nothing")
-        plt.close()
-        
-        pl.plot_kstest()
-        plt.close()
-        pl.plot_anderson()
-        plt.close()
-        pl.plot_av_std()
-        plt.close()
+        pl.plot_kstest(ax=ax)
+        pl.plot_anderson(ax=ax)
+        pl.plot_av_std(ax=ax)
+
+        ax.clear()
+        pl.sort(1)
+        fig = None
+        ax = None
+
+        pl.plot_spectra(fig=f)
+        pl.plot_spectra(fig=f,show_groups=True)
+        nt.assert_raises(ValueError,pl.plot_spectra,100,fig=f)
+
+        pl.plot_spectra_errs(fig=f)
+        pl.plot_spectra_errs(fig=f,show_groups=True)
+        nt.assert_raises(ValueError,pl.plot_spectra_errs,100,fig=f)
+
+        for mode in ["varsum","weightedsum","raw","norm"]:
+            pl.hist_2d(ax=ax,plottype = mode)
+
+        nt.assert_raises(NameError, pl.hist_2d, "nothing")
+        pl.plot_kstest(ax=ax)
+        pl.plot_anderson(ax=ax)
+        pl.plot_av_std(ax=ax)
--- hera_stats/tests/test_jackknife.py
+++ hera_stats/tests/test_jackknife.py
@@ -6,40 +6,40 @@ import numpy as np
 
 
 class Test_Jackknife():
-    
+
     def setUp(self):
-        
+        import matplotlib
+        matplotlib.use("Agg",warn=False)
+
         self.filepath = "./hera_stats/data/gaussian.N18.2018-06-06.06_15_48/"
-        
+
         self.jk = hs.jackknife()
         self.jk.load_uvd(self.filepath)
-        
+
         self.spw = [(600,700)]
         self.beampath = "./hera_stats/data/NF_HERA_Beams.beamfits"
         self.jk.calc_uvp(self.spw,beampath=self.beampath,baseline=(0,1))
-        
-    
+
     def test_init(self):
-        
+
         jk = hs.jackknife()
-        jk.load_uvd(self.filepath)
-        
+        jk.load_uvd(self.filepath,verbose=True)
         nt.assert_is_instance(jk.uvd, pyuvdata.UVData)
-        
+        jk.find_files("./hera_stats/data/",".jkf")
+
     def test_calc_uvp(self):
-        
+
         jk = self.jk
-        
+
         spw = self.spw
         bp = self.beampath
         bsl = (0,1)
         pols = ("XX","XX")
         use_ants = None
-        
-        jk.calc_uvp(spw,beampath=bp,baseline=bsl,pols=pols,use_ants=use_ants)
-        
-        nt.assert_is_instance(jk.uvp, hp.uvpspec.UVPSpec)
 
+        jk.calc_uvp(spw,beampath=bp,baseline=bsl,pols=pols,use_ants=use_ants)
+
+        nt.assert_is_instance(jk.uvp, hp.uvpspec.UVPSpec)
         nt.assert_raises(ValueError, jk.calc_uvp, spw, (0,3), pols, bp, 
                          "blackman-harris", use_ants=use_ants)
         nt.assert_raises(ValueError, jk.calc_uvp, spw,bsl, ("YY","YY"), bp, 
@@ -48,40 +48,40 @@ class Test_Jackknife():
                          "blackman-harris", use_ants=[0,1,2,3,4,5,6])
         nt.assert_raises(ValueError, jk.calc_uvp, spw, (1,11), pols, bp, 
                          "blackman-harris", use_ants=[0,1])
-        
+
     def test_split_ants(self):
-        
+
         jk = self.jk
-        
+
         uvpl, grps = jk.jackknives.split_ants()
         nt.assert_true(len(grps[0]) == len(grps[1]))
-                
+
         for i,uv in enumerate(uvpl):
             blpairs = np.unique(uv.blpair_array)
             ants = np.array([uv.blpair_to_antnums(b) for b in blpairs]).flatten()
             nt.assert_true(sum([a in grps[i] for a in ants]) == len(ants))
 
     def test_bootstrap(self):
-        
+
         jk = self.jk
         uvpl,grps = jk.jackknives.split_ants()
-        
+
         dlys,spectra,errs = jk.bootstrap_errs_once(uvpl[0])
         nt.assert_equal(dlys.shape,spectra.shape,errs.shape)
-        
+
         dlys,spectra,errs = jk.bootstrap_errs(uvpl)
         nt.assert_equal(len(spectra),2)
-        
+
     def test_jackknife_short(self):
-        
+
         jk = self.jk
-        
+
         nt.assert_raises(NameError, jk.jackknife, "nothing", 1, self.spw, self.beampath)
-        
+
         dic = jk.jackknife("split_ants", 1, self.spw, self.beampath, baseline=(0,1),
-                           n_boots = 10, returned = True)
-        
+                           n_boots = 10, returned = True,calc_avspec=True,verbose=True,
+                           savename="test")
+
         nt.assert_true("spectra" in dic.keys())
         nt.assert_true("dlys" in dic.keys())
         nt.assert_true("errs" in dic.keys())
-        
