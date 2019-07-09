5ed3bfd95089837d3818ca73aa975a693711d49a
Steven Murray <steven.murray@curtin.edu.au>
2019-07-09 14:22:51 -0700

    fix installation order issue

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup
-import sys
-import os
-from hera_stats import version
 import json
+import os
+import sys
+
+from setuptools import setup
+
+sys.path.append("hera_stats")
+import version
 
 data = [version.git_origin, version.git_hash, version.git_description, version.git_branch]
 with open(os.path.join('hera_stats', 'GIT_INFO'), 'w') as outfile:
     json.dump(data, outfile)
+
 
 def package_files(package_dir, subdirectory):
     # walk the input package_dir/subdirectory
@@ -18,22 +22,24 @@ def package_files(package_dir, subdirectory):
             path = path.replace(package_dir + '/', '')
             paths.append(os.path.join(path, filename))
     return paths
+
+
 data_files = package_files('hera_stats', 'data')
 
 setup_args = {
-    'name':         'hera_stats',
-    'author':       'HERA Team',
-    'url':          'https://github.com/HERA-Team/hera_stats',
-    'license':      'BSD',
-    'version':      version.version,
-    'description':  'HERA Jackknives and Statistical Analyses.',
-    'packages':     ['hera_stats'],
-    'package_dir':  {'hera_stats': 'hera_stats'},
+    'name': 'hera_stats',
+    'author': 'HERA Team',
+    'url': 'https://github.com/HERA-Team/hera_stats',
+    'license': 'BSD',
+    'version': version.version,
+    'description': 'HERA Jackknives and Statistical Analyses.',
+    'packages': ['hera_stats'],
+    'package_dir': {'hera_stats': 'hera_stats'},
     'package_data': {'hera_stats': data_files},
-    'install_requires': ['numpy>=1.10', 'scipy>=0.19', 'matplotlib',],
-    'extras_require': {'automate': ['jupyter',]},
+    'install_requires': ['numpy>=1.10', 'scipy>=0.19', 'matplotlib', ],
+    'extras_require': {'automate': ['jupyter', ]},
     'include_package_data': True,
-    'zip_safe':     False,
+    'zip_safe': False,
 }
 
 if __name__ == '__main__':
